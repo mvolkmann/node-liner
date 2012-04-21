e12764901667d680c242fe17d231b8c166973eb6
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2012-04-21 14:11:58 -0500

    modified so Liner can be used as a stream

--- test/liner.test.js
+++ test/liner.test.js
@@ -1,22 +1,37 @@
 'use strict';
-var liner = require('../lib/liner.js');
+var Liner = require('../lib/liner.js');
 
-exports.testFromPath = function (t) {
+function compareLines(t, lines) {
+  var len = lines.length;
+  t.equal(len, 25, 'number of lines read');
+  t.equal(lines[0],
+    'Come and listen to a story about a man named Jed',
+    'first line');
+  t.equal(lines[len - 1], "Y'all come back now, y'hear?.",
+    'last line');
+  t.done();
+}
+
+function testFromPath(t) {
   var lines = [];
-  liner.fromPath('../story.txt', function (err, line) {
+  Liner.fromPath('../story.txt', function (err, line) {
     if (err) {
       t.ifError(err);
     } else if (line === null) {
-      var len = lines.length;
-      t.equal(len, 25, 'number of lines read');
-      t.equal(lines[0],
-        'Come and listen to a story about a man named Jed',
-        'first line');
-      t.equal(lines[len - 1], "Y'all come back now, y'hear?.",
-        'last line');
-      t.done();
+      compareLines(t, lines);
     } else {
       lines.push(line);
     }
   });
 };
+
+function testUsingStream(t) {
+  var lines = [];
+  var liner = new Liner('../story.txt');
+  liner.on('data', lines.push.bind(lines));
+  liner.on('error', t.ifError);
+  liner.on('end', compareLines.bind(null, t, lines));
+};
+
+exports.testFromPath = testFromPath;
+exports.testUsingStream = testUsingStream;
